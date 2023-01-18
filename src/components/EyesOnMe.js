6dfa8e4f409471aae2415f92ded66697e417edae
GriffinsNgeno <griffins.ngeno@student.moringaschool.com>
2023-01-18 06:34:25 +0300

    Completed Assignment

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,17 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const EyesOnMe = () => {
+  return (
+    <div>
+        <button 
+            onBlur={() => console.log("Hey! Eyes on me!")}
+            onFocus={() => console.log("Good!")}
+            >
+                Eyes on me
+            </button>
+    </div>
+  )
+}
+
+export default EyesOnMe
